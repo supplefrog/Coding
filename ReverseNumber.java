deea0f240d0a147be221f4d1ceaaa7848ac20426
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-18 12:06:15 +0530

    Create ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        if(sc.hasNextInt()){
+            num = sc.nextInt();
+        }
+		int rem;
+        /*int numlen = Integer.valueOf(num).toString().length();
+        for(int i=1; i<=numlen; i++){
+            rem = num%10;
+            num = num/10;
+            System.out.print(rem);*/
+		for(; num>0;){
+            rem = num%10;
+            num = num/10;
+            System.out.print(rem);
+		}
+    }
+}
