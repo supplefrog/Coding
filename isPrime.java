3c6db1db1a731d6e39a4b1ffa814a5919d319661
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-18 20:39:17 +0530

    Update isPrime.java

--- isPrime.java
+++ isPrime.java
@@ -1,3 +1,5 @@
+//Condition for prime: two factors
+//Composite: more than two factors
 import java.util.Scanner;
 import java.lang.Math;
 class Main {
@@ -11,10 +13,17 @@ class Main {
 		for(int i = 2; i <= Math.sqrt(num); i++) {
             if(num % i == 0) {
                 isPrime = false;
+                break;
             }
         }
-        if (isPrime = false) {
-            System.out.println("not prime");
+        if(num < 0) {
+            System.out.println("Number must be natural");
+        }
+        else if(num == 0 || num == 1) {
+            System.out.println("neither");
+        }
+        else if (isPrime == false) {
+            System.out.println("composite");
         }
         else {
             System.out.println("is prime");
