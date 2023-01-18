bd6d22c5add70e3f2ef796046f8841f54127c2ac
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-18 21:58:30 +0530

    Update isPrime.java

--- isPrime.java
+++ isPrime.java
@@ -6,20 +6,20 @@ class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int num = 0;
-        if(sc.hasNextInt()) {
+        if (sc.hasNextInt()) {
             num = sc.nextInt();
         }
         boolean isPrime = true;
 		for(int i = 2; i <= Math.sqrt(num); i++) {
-            if(num % i == 0) {
+            if (num % i == 0) {
                 isPrime = false;
                 break;
             }
         }
-        if(num < 0) {
+        if (num < 0) {
             System.out.println("Number must be natural");
         }
-        else if(num == 0 || num == 1) {
+        else if (num == 0 || num == 1) {
             System.out.println("neither");
         }
         else if (isPrime == false) {
