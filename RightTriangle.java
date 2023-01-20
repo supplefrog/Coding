3f3f4d4690a090fb05f9c5cbc46e634f6c6f5cc4
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-20 18:26:37 +0530

    Update RightTriangle.java

--- RightTriangle.java
+++ RightTriangle.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 class Main {
     public static void main(String args[]) {
-        for(int line = 1; line <= 4; line++) { //condition works like an if (conditional) statement to break the loop
-            for(int star = 4; star >= line; star--) { //mutable variable in condition changes the no of times loop runs
+        //input
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        if (sc.hasNextInt()) {
+            num = sc.nextInt();
+        }
+        //code
+        /*
+        for(int line = 1; line <= num; line++) {
+            for(int star = num; star <= num - line + 1; star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        */
+        for(int line = 1; line <= num; line++) { //condition works like an if (conditional) statement to break the loop
+            for(int star = num; star >= line; star--) { //mutable variable in condition changes the no of times loop runs
                 System.out.print("*");
             }
             System.out.println();
