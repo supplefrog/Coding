24e57fff556ca71f7e8303331252eba8927fd705
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-20 19:07:00 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
 class Main {
     public static void main(String args[]) {
-        //input
         Scanner sc = new Scanner(System.in);
-        int num = 0;
-        if (sc.hasNextInt()) {
-            num = sc.nextInt();
-        }
-        //code
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
         if (num > 1) {
             for (int num_minus = num - 1; num_minus > 1; num--) {
                 num = num * num_minus;
