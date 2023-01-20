45db74a1189cd627dabaff00146e7301fa455552
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-20 18:58:30 +0530

    Update InvertNumberTriangle.java

--- InvertNumberTriangle.java
+++ InvertNumberTriangle.java
@@ -6,11 +6,12 @@ class Main {
         System.out.print("Enter the max number: ");
         int max = input.nextInt();
 
+        System.out.println();
         for(int line = max; line >= 1; line--) {
             for(int number = line; number >= 1; number--) {
                 System.out.print(number);
             }
-            System.out.println();
+        System.out.println();
         }
     }
 }
