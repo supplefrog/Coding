61b0ad9240a3798bffb7f1d7e4ac3fcf740f4d08
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-20 18:44:33 +0530

    Update and rename InvertedRightTriangle.java to InvertNumberTriangle.java

---
+++ InvertNumberTriangle.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the max number: ");
+        int max = input.nextInt();
+
+        for(int line = max; line >= 1; line--) {
+            for(int number = line; number >= 1; number--) {
+                System.out.print(number);
+            }
+            System.out.println();
+        }
+    }
+}
