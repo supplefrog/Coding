ad2447f41e6240dbfab57bfdc9f783a9a616ca8d
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-20 18:57:20 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-	System.out.print("Enter a number: ");
+        System.out.print("Enter a number: ");
         int num = sc.nextInt();
-	
-	int rem;
+
+        System.out.println();
         /*
 	int numlen = Integer.valueOf(num).toString().length();
         for (int i = 1; i <= numlen; i++) {
-	    rem = num % 10;
-	    num = num / 10;
+            int rem = num % 10;
+            num = num / 10;
             System.out.print(rem);
 	}
 	*/
 	for (; num>0;) {
-            rem = num % 10;
+	    int rem = num % 10;
             num = num / 10;
             System.out.print(rem);
 	}
