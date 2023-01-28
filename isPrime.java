149cd6a142c55c01f942e34370ca6c94dc46dc34
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-28 18:48:02 +0530

    Update isPrime.java

--- isPrime.java
+++ isPrime.java
@@ -1,34 +1,43 @@
-//Prime - two factors
-//Composite - more than two factors
 import java.util.Scanner;
 import java.lang.Math;
 
 class Main {
-    public static void isPrime(num) {
-	boolean isPrime = true;
-	for(int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (num < 0) {
-            System.out.println("Number must be natural");
-        }
-        else if (num == 0 || num == 1) {
-            System.out.println("neither");
-        }
-        else if (isPrime == false) {
-            System.out.println("composite");
-        }
-        else {
-            System.out.println("is prime");
+    public static class IntegerException extends Exception {
+        public IntegerException(String message) {
+            super(message);
         }
     }
+
+    public static boolean isPrime(int num) throws IntegerException {
+        if (num == 0 || num == 1) {
+            throw new IntegerException("Neither prime nor composite");
+        } 
+        else if (num < 0) {
+            throw new IntegerException("Enter a natural number");
+        }
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-	System.out.print("Enter a number: ");
+        System.out.println("Enter a natural number: ");
         int num = sc.nextInt();
-	isPrime(num);
+        try {
+            boolean isPrime = isPrime(num);
+            if (isPrime) {
+                System.out.println("Prime");
+            } 
+	    else {
+                System.out.println("Composite");
+            }
+        } 
+	catch (IntegerException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
