6ef4c7b77dac87fc194dfde63aed499f9954fe11
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-28 16:20:48 +0530

    Update InvertNumberTriangle.java

--- InvertNumberTriangle.java
+++ InvertNumberTriangle.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 
 class Main {
-    public static void main(String args[]) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the max number: ");
-        int max = input.nextInt();
-
+    public static void invNumTriangle(int max) {
         for(int line = max; line >= 1; line--) {
             System.out.println();
             for(int number = line; number >= 1; number--) {
                 System.out.print(number);
             }
         }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the max number: ");
+        int max = sc.nextInt();
+        
+        invNumTriangle(max);
     }
 }
