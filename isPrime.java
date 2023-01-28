20a51c6dd596b9d379597bc7da5682e63132fb4c
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-28 19:39:18 +0530

    Update isPrime.java

--- isPrime.java
+++ isPrime.java
@@ -7,14 +7,10 @@ class Main {
             super(message);
         }
     }
-
     public static boolean isPrime(int num) throws IntegerException {
-        if (num == 0 || num == 1) {
+        if (num == 1) {
             throw new IntegerException("Neither prime nor composite");
-        } 
-        else if (num < 0) {
-            throw new IntegerException("Enter a natural number");
-        }
+	}
         for(int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
@@ -22,11 +18,13 @@ class Main {
         }
         return true;
     }
-
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a natural number: ");
-        int num = sc.nextInt();
+        int num = -1;
+        while (num < 1) {
+            System.out.println("Enter a natural number: ");
+            num = sc.nextInt();
+        }
         try {
             boolean isPrime = isPrime(num);
             if (isPrime) {
