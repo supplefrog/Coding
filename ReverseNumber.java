76b9eee097c623c419239c482c6880dfaa349ace
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-29 19:43:27 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -2,17 +2,8 @@ import java.util.Scanner;
 
 class Main {
     public static void reverse(int num) {
-    	/*
-	int numlen = Integer.valueOf(num).toString().length();
-        for (int i = 1; i <= numlen; i++) {
-            int rem = num % 10;
-            num = num / 10;
-            System.out.print(rem);
-	}
-	*/
-	for (; num>0;) {
+	for (; num > 0; num /= 10) {
 	    int rem = num % 10;
-            num = num / 10;
             System.out.print(rem);
 	}
     }	    
