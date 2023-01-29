b6156fd53ecaf47412f5b654729e049ed9d20573
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-29 19:32:52 +0530

    Create isPalindrome.java

---
+++ isPalindrome.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Main {
+    public static int reverse(int num) {
+        int reverse = 0;
+        for (int aux = num; aux > 0; aux /= 10) {
+            reverse = reverse * 10 + aux % 10;
+        }
+        return (reverse);
+    }
+    public static void isPalindrome(int num) {
+        if (num == reverse(num)) {
+            System.out.println("Palindrome");
+        }
+        else {
+            System.out.println("Not a palindrome");
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int num = sc.nextInt();
+        isPalindrome(num);
+    }
+}
