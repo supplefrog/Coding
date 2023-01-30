4e3e23123f65be56fdf64a00c51491a0c1053d61
supplefrog <78985073+supplefrog@users.noreply.github.com>
2023-01-30 22:12:33 +0530

    Update PrimesInInterval.java

--- PrimesInInterval.java
+++ PrimesInInterval.java
@@ -2,25 +2,6 @@ import java.util.Scanner;
 import java.lang.Math;
 
 class Main {
-    public static boolean isPrime(int num) {
-        if (num == 1) {
-            System.out.println("Neither prime nor composite");
-            return false;
-        }
-        for(int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-    public static void primesInInterval(int lb, int ub) {
-        for (int n = lb; n <= ub; n++) {
-            if (isPrime(n)) {
-                System.out.print(n + " ");
-            }
-        }
-    }
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int lb = -1, ub = -1;
@@ -33,5 +14,26 @@ class Main {
             ub = sc.nextInt();
         }
         primesInInterval(lb, ub);
+    }
+
+    public static void primesInInterval(int lb, int ub) {
+        for (int n = lb; n <= ub; n++) {
+            if (isPrime(n)) {
+                System.out.print(n + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num == 1) {
+            System.out.println("Neither prime nor composite");
+            return false;
+        }
+        for(int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
